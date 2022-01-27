544bd6be571e15a2748c9f4dfab20f10cd70d497
yasinkrcn <23yasin1999@gmail.com>
2022-01-27 20:33:43 +0300

    splash secreen yapıldı dark mode lightmode eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -19,14 +19,56 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Spotify',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const ChooseArtist());
+    return FutureBuilder(
+        future: Init.instance.initialize(),
+        builder: (context, AsyncSnapshot snapshot) {
+          // Show splash screen while waiting for app resources to load:
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const MaterialApp(home: Splash());
+          } else {
+            // Loading is done, return the app:
+            return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                title: 'Spotify',
+                theme: ThemeData(
+                  primarySwatch: Colors.blue,
+                ),
+                home: const ChooseArtist());
+          }
+          ;
+        });
+  }
+}
+
+class Splash extends StatelessWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    bool lightMode =
+        MediaQuery.of(context).platformBrightness == Brightness.light;
+    return Scaffold(
+      backgroundColor:
+          lightMode ? const Color(0xffe1f5fe) : const Color(0xff042a49),
+      body: Center(
+          child: lightMode
+              ? Image.asset('assets/splash_screen/spotify_splash.png')
+              : Image.asset('assets/splash_screen/spotify_splash_dark.png')),
+    );
+  }
+}
+
+class Init {
+  Init._();
+  static final instance = Init._();
+
+  Future initialize() async {
+    // This is where you can initialize the resources needed by your app while
+    // the splash screen is displayed.  Remove the following example because
+    // delaying the user experience is a bad design practice!
+    await Future.delayed(const Duration(seconds: 3));
   }
 }
